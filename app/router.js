516551592ce987a482523b4e2d58da1282606b98
Allan <allan.mclelland@gmail.com>
2016-06-17 16:47:37 -0600

    Meal Show page

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('meals');
+  this.route('meal', { path: '/meal/:meal_id' });
   this.route('not-found', { path: '/*path' });
 });
 
