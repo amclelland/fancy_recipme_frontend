f59efdbd222629c29f85ceb71902e0530f7b0497
Allan <allan.mclelland@gmail.com>
2016-06-18 16:40:45 -0600

    Try to talk to API

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,7 @@ import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import Ember from 'ember';
 
 export default JSONAPIAdapter.extend({
+  host: 'http://recipme-rails.herokuapp.com',
   pathForType: function(type) {
     return Ember.String.pluralize(Ember.String.underscore(type));
   }
