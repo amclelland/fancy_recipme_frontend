4e9d6b50cdc5b475901509ef792c933380e085a5
Allan <allan.mclelland@gmail.com>
2016-06-18 21:51:30 -0600

    Add dev and prod hosts

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,9 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import Ember from 'ember';
+import ENV from '../config/environment';
 
 export default JSONAPIAdapter.extend({
-  host: 'http://recipme-rails.herokuapp.com',
+  host: ENV.host,
   pathForType: function(type) {
     return Ember.String.pluralize(Ember.String.underscore(type));
   }
